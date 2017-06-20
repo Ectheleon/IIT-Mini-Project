a5521e37a60b8e73c88fab3551ca2e616cbcbcb4
Jonathan Peters <peters@maths.ox.ac.uk>
2017-06-20 16:05:58 +0100

    Finished functions which quantify a network classically. Now to run numerical experiments to compare the classical results to IIT for different connectivity matrices.

--- Code/2ClusterModel.py
+++ Code/2ClusterModel.py
@@ -11,8 +11,31 @@ import pyphi as phi
 import matplotlib.pyplot as plt
 import itertools
 from numba import jit
+import scipy as sp;
 
-states = np.array(list(itertools.product([0,1], repeat = 5)))
+states = np.array(list(itertools.product([0,1], repeat = 8)))
+costs = np.array([.45,.3, .25]);
+
+probs = np.array([0.5,.5,.5])
+
+team_skill_map = np.zeros([5, 3])
+team_skill_map[0,0] = 1;
+team_skill_map[1,1] = 1;
+team_skill_map[2,0], team_skill_map[2,1] = 1,1;
+team_skill_map[3,2], team_skill_map[3,0] = 1,1;
+team_skill_map[4,2], team_skill_map[4,1] = 1,1;
+
+                                         
+cm = np.zeros([5,5],dtype = int)
+#cm[0,1] = 1
+cm[0,2], cm[1,0], cm[1,2], cm[2,0], cm[2,1] = 1,1,1,1,1
+cm[3,4] = 1
+cm[4,3]=1
+k = [.2, .2, .1, .05, .05]
+cm[0,0] , cm[1,1], cm[2,2], cm[3,3],cm[4,4] = 1,1,1,1,1
+cm[3,0],cm[3,1], cm[4,0], cm[4,1]=1,1,1,1
+
+k = np.divide(np.ones([8]),8)
 
 
 @jit
@@ -58,8 +81,43 @@ def stat_dist(tpm):
         print(eig)
         print(tpm)
 
+def connection_options(nedges):
+    cm = np.eye(5);
+    
+    if (nedges<5) | (nedges>25):
+        print('error, the network must have between 5 and 25 edges inclusive')
+    
+    nopts = sp.misc.comb(20,nedges)
+    opts = np.empty([int(nopts), 5,5], dtype = int)
 
-#%%
+    #create a length 20 array of 0's;
+    arr = np.zeros([20])
+    #loop through all places to place ones.
+    
+    k = 0
+    for i in itertools.combinations(range(20),nedges):
+        arr.fill(0)
+        #fill in the 1's
+        for j in i:
+            arr[j] = 1;
+        #reshape
+        temp = np.reshape(arr, [5,4]);
+        
+        cm[0,1:] = temp[0];
+        cm[1,0] = temp[1,0];
+        cm[1,2:] = temp[1,1:]
+        cm[2,0:2] = temp[2,0:2]
+        cm[2,3:] = temp[2,2:]
+        cm[3,:3] = temp[3, :3]
+        cm[3,4] = temp[3,3]
+        cm[4,:4] =temp[4,:4]
+
+
+        opts[k]= cm;
+        k+=1
+    return opts
+
+
 
 def buildTPM(cm, probs, k):
     nnodes = 5;
@@ -187,62 +245,133 @@ def buildTPM(cm, probs, k):
             
             TPM[row+32*project_index, 0:5] = A,B,C,D,E
             TPM = np.round(TPM, 3)
-            print(i,s, project, np.round(TPM[row+32*project_index, :5],3))
+            #print(i,s, project, np.round(TPM[row+32*project_index, :5],3))
             i+=1
             
     
     
     return TPM,CM
     
-                                                 
-cm = np.zeros([5,5],dtype = int)
-cm[0,1], cm[0,2], cm[1,0], cm[1,2], cm[2,0], cm[2,1] = 1,1,1,1,1,1
-cm[3,4] = 1
-cm[4,3]=1
-k = [.2, .2, .1, .05, .05]
 
-#%%
-def classical(TPM, state, costs):
+
+def classical(TPM,skill_map, state, costs):
     #computes the cost of waste when the system transitions out of 'state'.
+    waste = 0;
     
     old = state;
     ind = np.sum([2**i * old[i] for i in range(8)]);
 
     new = np.ceil(TPM[ind]);
     new_workers = new[:5]-old[:5];
-    print(new_workers)
+    
+    used_skills = new_workers.dot(skill_map);
+    needed_skills = old[5:]
+    
+    
+    #How many times are we doing this project?
+    
+    if np.sum(needed_skills)>0:
+        if(np.sum(used_skills)>0):
+            n = 0;
+            
+            while (used_skills-n*needed_skills>=0).all():
+                n+=1;
+            #now we know the project is being done n times.
+            
+            ok_excess = used_skills-(n-1)*needed_skills;
+            if n>1:
+                bad_excess = (n-2)*needed_skills;
+            else:
+                bad_excess = 0*needed_skills
+            
+            #we've allocated the project well, now the only waste is excess skills.
+            for i in range(3):
+                #we only count 10% of ok excess
+                waste+= costs[i]*ok_excess[i]*0.1
+                
+                #we count the full amount of bad excess
+                waste +- costs[i]*bad_excess[i]
+                
+        else:
+            #now the question is, does there exist a team which should have 
+            #taken up the new project?
+            
+            #do the required skills exist?
+            if ((1-old[:5]).dot(skill_map)-needed_skills>=0).all():
+                for i in range(3):
+                    waste+=10*costs[i]*needed_skills[i];
+
+    
+    return waste;
+
+def quantify_network(cm, probs, k):
+    TPM, CM = buildTPM(cm,probs, k);
+    net = phi.Network(TPM, CM)
+    
+    dat = np.zeros([2**8, 2])
+
+    j= 0;
+    for state in states:
+        state = state[::-1]
+        waste = (classical(TPM, team_skill_map, state,costs))
+        cmplxs = phi.compute.condensed(net,state);
+        
+        phis = []
+        for i in range(len(cmplxs)):
+            phis.append(cmplxs[i].phi)
+        dat[j,:] = np.array([waste, np.sum(phis)])
+        #print(j,waste, phis)
+        j+=1
+        
+    dist = np.real(stat_dist(transform_tpm(TPM)));
+    
+    return dist.dot(dat)
     
 #%%
-states = np.array(list(itertools.product([0,1], repeat = 8)))
+nedges = 10;
+cms = connection_options(nedges);
+
+dat = [];
+for i in range(10):
+    print('\n\nStarting calculation for new connectivity matrix')
+    a,b = quantify_network(cms[i], probs, k)
+    dat.append([a,b])
+    print(cms[i], a, b)
 
 
-TPM,CM = buildTPM(cm, (.5,.5,.5),k )
+
+
+
+
+
+
 
 
 
 
 #%%
+TPM, CM = buildTPM(cm, probs, k)
 net = phi.Network(TPM, CM);
-sub = phi.Subsystem(net, [0,1,1,0,1,1,0,0], range(8))
-cmplxs = phi.compute.condensed(net, [0,1,1,0,1,1,0,0])
-#%%
 
-dat1 = np.zeros([2**8])
-dat2 = np.zeros([2**8])
+dat = np.zeros([2**8, 2])
 
-
-
-
-net = phi.Network(TPM, CM)
+j= 0;
 for state in states:
     state = state[::-1]
-    dat = phi.compute.main_complex(net, state).phi
-    tmp = phi.compute.condensed(net, state)
+    waste = (classical(TPM, team_skill_map, state,costs))
+    cmplxs = phi.compute.condensed(net,state);
     
-    print(state, dat, len(tmp))
+    phis = []
+    for i in range(len(cmplxs)):
+        phis.append(cmplxs[i].phi)
+    dat[j,:] = np.array([waste, np.sum(phis)])
+    print(waste, phis)
+    j+=1
+    
+#%%
+plt.scatter(dat[:,0], dat[:,1])
 
 
-
-
-
-
+#%%
+full_TPM = transform_tpm(TPM)
+avg_dist = np.real(stat_dist(full_TPM))
